adf0cfc479fa642e5955bbe51cd0fad386a8734a
Tiffany Thompson <tdthompson899@gmail.com>
2018-07-05 23:01:46 -0500

    starting code for music app

---
+++ app/Http/Controllers/SpotifyController.php
@@ -0,0 +1,9 @@
+<?php namespace App\Http\Controllers;
+
+class SpotifyController extends Controller
+{
+    public function __construct()
+    {
+        // use client id and secret to call spotify api
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Music App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Material Styles -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+        <link rel="stylesheet" href="/css/materialize.css">
+    </head>
+    <body>
+       <h1>Welcome to my Music Quiz</h1>
+
+        <h4>Try your hand at guessing the songs </h4>
+        <iframe src="https://open.spotify.com/embed?uri=spotify:user:tdthompson899:playlist:2VpMOVLzcfNgrl5aLbaa3p" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+       <!-- Material JavaScripts -->
+       <script src="/js/materialize.js"></script>
+    </body>
+</html>
